40a35a2812026c2fd95a467ba7512684800bc7b7
Radek Novacek <rad.n@centrum.cz>
2022-09-21 10:50:47 +0200

    add retrying when importing to victoriametrics

--- import/victoria.py
+++ import/victoria.py
@@ -1,3 +1,4 @@
+import asyncio
 import csv
 from typing import Dict, List, Sequence, TypedDict, Union
 
@@ -17,13 +18,29 @@ class Metric(TypedDict):
 
 
 class Victoria(OutputBase):
+    MAX_ATTEMPTS = 3
     def __init__(self, url: str):
         self.url = url
 
     async def import_data(self, metrics: Sequence[bytes]):
-        async with aiohttp.ClientSession() as session:
-            response = await session.post(f'{self.url}/api/v1/import', data=b'\n'.join(metrics))
-            print(response.status)
-            response.raise_for_status()
-            if response.content_length:
-                print(response.content)
+        for attempt in range(1, self.MAX_ATTEMPTS + 1):
+            async with aiohttp.ClientSession() as session:
+                try:
+                    response = await session.post(f'{self.url}/api/v1/import', data=b'\n'.join(metrics))
+                except aiohttp.ClientError:
+                    print(f'{attempt}/{self.MAX_ATTEMPTS} Cannot connect to victoriametrics')
+                    if attempt < self.MAX_ATTEMPTS:
+                        await asyncio.sleep(2 ** attempt)
+                        continue
+                    else:
+                        raise
+                else:
+                    if not response.ok:
+                        print(f'{attempt}/{self.MAX_ATTEMPTS} Cannot send data to victoriametrics, status {response.status}')
+                        if attempt < self.MAX_ATTEMPTS:
+                            await asyncio.sleep(2 ** attempt)
+                            continue
+                        else:
+                            response.raise_for_status()
+                    else:
+                        break
